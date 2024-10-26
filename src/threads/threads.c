ad0d6a7d1c791472c245bfdd5638aa71803a4ef8
ecortes- <ecortes-@IdeapadEdu>
2024-10-26 18:42:33 +0200

    a

--- src/threads/threads.c
+++ src/threads/threads.c
@@ -6,7 +6,7 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:31:26 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/10/26 18:38:46 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/10/26 18:42:13 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,6 @@ void	set_dead_flag(int i, t_philos *ph)
 
 int	is_dead_flag(t_philos *ph)
 {
-	int	i;
-
 	pthread_mutex_lock(ph->dead_lock);
 	if (*ph->dead_flag == 1)
 	{
