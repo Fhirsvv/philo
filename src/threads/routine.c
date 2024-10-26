29fb4f777d54a24e66c9920051926143b22d2691
ecortes- <ecortes-@IdeapadEdu>
2024-10-26 19:24:26 +0200

    ns como hacerlo mas rapido

--- src/threads/routine.c
+++ src/threads/routine.c
@@ -6,16 +6,16 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:42:02 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/10/26 18:29:46 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/10/26 19:16:40 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-static void	phwrite(char *str, size_t time, int id, t_philos *ph)
+static void	phwrite(char *str, size_t time, t_philos *ph)
 {
 	pthread_mutex_lock(ph->write_lock);
-	printf(str, time, id);
+	printf(str, time, ph->id);
 	pthread_mutex_unlock(ph->write_lock);
 }
 
@@ -23,21 +23,23 @@ static int	eat(t_philos *ph)
 {
 	size_t	time;
 
+	if (is_dead_flag(ph))
+		return (1);
 	pthread_mutex_lock(ph->right_fork);
 	pthread_mutex_lock(ph->left_fork);
 	time = get_current_time() - ph->start_time;
-	if (is_dead_flag(ph))
-		return (1);
 	if (*ph->max_meals == ph->current_meals)
 	{
 		set_dead_flag(1, ph);
 		dead_write(ph);
+		pthread_mutex_unlock(ph->right_fork);
+		pthread_mutex_unlock(ph->left_fork);
 		return (1);
 	}
 	else
-		phwrite("%zu %d is eating\n", time, ph->id, ph);
+		phwrite("%zu %d is eating\n", time, ph);
 	pthread_mutex_lock(ph->meals_lock);
-	ph->current_meals++;
+	++ph->current_meals;
 	ph->time_lmeal = get_current_time() - ph->start_time;
 	pthread_mutex_unlock(ph->meals_lock);
 	ft_usleep(*ph->time_eat);
@@ -50,11 +52,10 @@ static int	think(t_philos *ph)
 {
 	size_t	time;
 
-	time = get_current_time() - ph->start_time;
 	if (is_dead_flag(ph))
 		return (1);	
-	else
-		phwrite("%zu %d is thinking\n", time, ph->id, ph);
+	time = get_current_time() - ph->start_time;
+	phwrite("%zu %d is thinking\n", time, ph);
 	if (time - ph->time_lmeal >= *ph->time_die)
 	{
 		set_dead_flag(1, ph);
@@ -68,17 +69,10 @@ static int	phsleep(t_philos *ph)
 {
 	size_t	time;
 
-	time = get_current_time() - ph->start_time;
 	if (is_dead_flag(ph))
 		return (1);
-	if (time - ph->time_lmeal >= *ph->time_die)
-	{
-		set_dead_flag(1, ph);
-		dead_write(ph);
-		return (1);	
-	}
-	else
-		phwrite("%zu %d is sleeping\n", time, ph->id, ph);
+	time = get_current_time() - ph->start_time;
+	phwrite("%zu %d is sleeping\n", time, ph);
 	ft_usleep(*ph->time_sleep);
 	time = get_current_time() - ph->start_time;
 	if  (time - ph->time_lmeal > *ph->time_die)
@@ -97,42 +91,9 @@ void	*routine(void *philo)
 	ph = (t_philos *)philo;
 	while (true)
 	{
-		if (!is_dead_flag(ph))
-		{
-			if (!eat(ph))
-			{
-				if (!phsleep(ph))
-				{
-					if (!think(ph))
-						continue;
-				}
-			}
-		}
-		set_dead_flag(1, ph);
+		if (!eat(ph) && !phsleep(ph) && !think(ph))
+			continue;
 		return (NULL);
 	}
 	return (NULL);
 }
-
-/*
-		if (is_dead_flag(ph))
-		{
-			dead_write(ph);
-			return (NULL);
-		}
-		if (eat(philo))
-		{
-			dead_write(ph);
-			return (NULL);
-		}
-		if (Psleep(philo))
-		{
-			dead_write(ph);
-			return (NULL);
-		}
-		if (think(philo))
-		{
-			dead_write(ph);
-			return (NULL);
-		}
-*/
--- src/threads/threads.c
+++ src/threads/threads.c
@@ -6,7 +6,7 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:31:26 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/10/26 18:42:13 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/10/26 19:18:16 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	threads(t_program *pr)
 		i++;
 	}
 	i = 0;
-	while (++i < pr->nb_philos)
+	while (i < pr->nb_philos)
 	{
 		pthread_join(pr->philos[i].thread, NULL);
 		i++;
@@ -42,12 +42,10 @@ void	set_dead_flag(int i, t_philos *ph)
 
 int	is_dead_flag(t_philos *ph)
 {
+	int	i;
+
 	pthread_mutex_lock(ph->dead_lock);
-	if (*ph->dead_flag == 1)
-	{
-		pthread_mutex_unlock(ph->dead_lock);
-		return (1);	
-	}
+	i = *ph->dead_flag;
 	pthread_mutex_unlock(ph->dead_lock);
-	return (0);
+	return (i);	
 }
