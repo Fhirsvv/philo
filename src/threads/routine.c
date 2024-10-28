1bee29aabcf753138ade1f53eb599756601c0103
ecortes- <ecortes-@IdeapadEdu>
2024-10-28 10:12:09 +0100

    imprime cuando cogen un fork

--- src/threads/routine.c
+++ src/threads/routine.c
@@ -6,7 +6,7 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:42:02 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/10/28 09:38:54 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/10/28 10:11:20 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,11 @@ static int	eat(t_philos *ph)
 	if (is_dead_flag(ph))
 		return (1);
 	pthread_mutex_lock(ph->right_fork);
+	time = get_current_time() - ph->start_time;
+	phwrite("%zu %d has taken a fork\n", time, ph);
 	pthread_mutex_lock(ph->left_fork);
+	time = get_current_time() - ph->start_time;
+	phwrite("%zu %d has taken a fork\n", time, ph);
 	pthread_mutex_lock(ph->meals_lock);
 	time = get_current_time() - ph->start_time;
 	phwrite("%zu %d is eating\n", time, ph);
