4923c9df630fc015f893ff6655c02a7a7adbafd5
Eduardo Cortes Navarro <ecortes-@c2r3s4.42madrid.com>
2024-10-29 16:09:23 +0100

    one philo done

--- src/threads/threads.c
+++ src/threads/threads.c
@@ -6,22 +6,31 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:31:26 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/10/29 16:01:12 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/10/29 16:09:02 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
+static void	*one_philo(void *philo)
+{
+	t_philos *ph;
+
+	ph = (t_philos *)philo;
+	phwrite("%zu %d has taken a fork\n", time, ph);
+	ft_usleep(ph->time_die);
+	dead_write(ph);
+	return (NULL);
+}
+
 void	threads(t_program *pr)
 {
 	size_t		i;
-	pthread_t	observer;
+	pthread_t	one_philo;
 
 	i = 0;
-	/*if (pthread_create(&observer, NULL, &controler, (void *)pr) != 0)
-		return ;*/
 	if (pr->nb_philos == 1)
-		pthread_create()
+		pthread_create(&one_philo, NULL, &one_philo, (void *)&pr->philos[0]);
 	while (i < pr->nb_philos)
 	{
 		if (pthread_create(&pr->philos[i].thread, NULL, &routine,
@@ -30,7 +39,6 @@ void	threads(t_program *pr)
 		i++;
 	}
 	i = 0;
-	pthread_join(observer, NULL);
 	while (i < pr->nb_philos)
 	{
 		pthread_join(pr->philos[i].thread, NULL);
